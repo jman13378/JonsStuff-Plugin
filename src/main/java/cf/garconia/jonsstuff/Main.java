f0c15ef55589bc0873908c0ee689c4ba92c3f6f0
jman13378 <garconiamail@gmail.com>
2022-04-05 19:43:07 -0400

    Add files via upload

--- src/main/java/cf/garconia/jonsstuff/Main.java
+++ src/main/java/cf/garconia/jonsstuff/Main.java
@@ -1,9 +1,9 @@
 package cf.garconia.jonsstuff;
 
 import java.net.http.WebSocket.Listener;
+import java.util.Arrays;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -20,32 +20,23 @@ public class Main extends JavaPlugin implements Listener, org.bukkit.event.Liste
 
 	@Override
 	public void onEnable() {
-		Commands commands = new Commands();
-		getServer().getPluginManager().registerEvents(new Events(), this);
-		itemManager.init();
-		config.addDefault("heal", true);
-		config.addDefault("feed", true);
-		config.addDefault("farmtime", true);
-		config.addDefault("givewand", true);
-        config.options().copyDefaults(true);
-		if (config.getBoolean("heal")) {
-			getCommand("heal").setExecutor(commands);
-		}
-		if (config.getBoolean("feed")) {
-			getCommand("feed").setExecutor(commands);
-		}
-		if (config.getBoolean("farmtime")) {
-			getCommand("heal").setExecutor(commands);
-		}
-		if (config.getBoolean("givewand")) {
-			getCommand("givewand").setExecutor(commands);
-		} 
-		getCommand("RLD").setExecutor(commands);
-        saveConfig();
-     // Enable our class to check for new players using onPlayerJoin()
-        getServer().getPluginManager().registerEvents(this, this);
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Jons-Stuff] Plugin Is Enabled!");
+	    saveDefaultConfig();
+	    itemManager.init();
+	    getServer().getPluginManager().registerEvents(new Events(), this);
+	    
+	    Commands commands = new Commands();
+	    for (String cmd : Arrays.asList("heal","feed","farmtime","givewand","RLD")) {
+	    	getCommand(cmd).setExecutor(commands);
+	    
+	      
+	    }
+	    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Jons-Stuff] Plugin Is Enabled!");
 	}
+	  Main plugin;
+
+	  public void Commands(Main plugin) {
+	    this.plugin = plugin;
+	  }
 	
 	@Override
 	public void onDisable() {
--- src/main/java/cf/garconia/jonsstuff/commands/Commands.java
+++ src/main/java/cf/garconia/jonsstuff/commands/Commands.java
@@ -5,13 +5,16 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import cf.garconia.jonsstuff.items.itemManager;
 
-public class Commands implements CommandExecutor {
-	
+public class Commands extends JavaPlugin implements CommandExecutor {
+	FileConfiguration config = getConfig();
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player)) { 
 			sender.sendMessage(ChatColor.RED + "Only Players Can execute This Command");
@@ -20,9 +23,15 @@ public class Commands implements CommandExecutor {
 		Player player = (Player) sender;
 		
 		if (cmd.getName().equalsIgnoreCase("heal")) {
-			double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
-			player.setHealth(maxHealth);
-			player.sendMessage("§e§l(!) §eHealth Filled");
+			if (player.hasPermission("jonsstuff.command.heal")) {
+				double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
+				player.setHealth(maxHealth);
+				player.sendMessage("§e§l(!) §eHealth Filled");
+				return true;
+			} else {
+				player.sendMessage("§c§l(!) §cMissing Permission");
+				player.sendMessage("§c§l(!) §cPermission: jonsstuff.command.heal");
+			}
 		}
 		else if (cmd.getName().equalsIgnoreCase("feed")) {
 			player.setFoodLevel(20);
@@ -47,6 +56,12 @@ public class Commands implements CommandExecutor {
 		else if (cmd.getName().equalsIgnoreCase("givewand")) {
 			player.getInventory().addItem(itemManager.wand);
 			player.sendMessage("§e§l(!) §eGiven Wand");
+		}
+		
+		else if (cmd.getName().equalsIgnoreCase("rld")) {
+			if (player.hasPermission("jonsstuff.command.reload")) {
+				reloadConfig();
+			}
 		}
 		return true;
 	}
